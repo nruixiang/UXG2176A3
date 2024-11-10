538388395ef8fbc102ad7443f21a7af758841422
nruixiang <ruidiotxienon@gmail.com>
2024-11-10 17:28:29 +0800

    meh got movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.TextCore.Text;
+
+public class Player : MonoBehaviour
+{
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    private float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = gameObject.AddComponent<CharacterController>();
+        speed = 2f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool isGrounded = controller.isGrounded;
+        if(isGrounded && playerVelocity.y < 0){
+            playerVelocity.y = 0f;
+        }
+
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        controller.Move(move * Time.deltaTime * speed);
+
+        // if(move != Vector3.zero){
+        //     gameObject.transform.forward = move;
+        // }
+    }
+
+}
