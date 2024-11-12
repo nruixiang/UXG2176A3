3f5b72638f772f2d4c075846fb076fc1ea11e73c
PeckHsia <peckhsia.teo@digipen.edu>
2024-11-12 12:39:42 +0800

    Camera & Player Movement

---
+++ Assets/Scripts/FPSController.cs
@@ -0,0 +1,132 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(CharacterController))]  
+public class FPSController : MonoBehaviour
+{
+    public Camera playerCamera;
+    public float walkSpeed = 6f;
+    public float runSpeed = 12f;
+    public float jumpPower = 7f;
+    public float gravity = 10f;
+
+    public float lookSpeed = 2f;
+    public float lookXLimit = 45f;
+
+    Vector3 moveDirection = Vector3.zero;
+    float rotationX = 0;
+
+    public bool canMove = true;
+
+    CharacterController characterController;
+
+    private bool doubleJump;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked; //Lock the cursor
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        #region Movement
+        bool isRunning = Input.GetKey(KeyCode.LeftShift);
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        Vector3 right = transform.TransformDirection(Vector3.right);
+
+        float curSpeedX = 0; // Initialize with default value
+        if (canMove)
+        {
+            if (isRunning)
+            {
+                curSpeedX = runSpeed * Input.GetAxis("Vertical");
+            }
+            else
+            {
+                curSpeedX = walkSpeed * Input.GetAxis("Vertical");
+            }
+        }
+        else
+        {
+            curSpeedX = 0; // This line is technically redundant since curSpeedX is already initialized to 0
+        }
+
+        float curSpeedY = 0; // Initialize with default value
+        if (canMove)
+        {
+            if (isRunning)
+            {
+                curSpeedY = runSpeed * Input.GetAxis("Horizontal");
+            }
+            else
+            {
+                curSpeedY = walkSpeed * Input.GetAxis("Horizontal");
+            }
+        }
+        else
+        {
+            curSpeedY = 0; // Again, redundant since curSpeedY starts at 0
+        }
+
+        float movementDirectionY = moveDirection.y;
+        moveDirection = (forward * curSpeedX) + (right * curSpeedY);
+        #endregion
+
+        #region Jumping
+
+        if(characterController.isGrounded && !Input.GetButtonDown("Jump"))
+        {
+            doubleJump = false;
+        }
+
+        if(Input.GetButtonDown("Jump") && canMove)
+        {
+            if(characterController.isGrounded || doubleJump)
+            {
+                Debug.Log("JUMPING TWICE");
+                moveDirection.y = jumpPower;
+                doubleJump = !doubleJump;
+            }
+        }
+
+      
+        else
+        {
+            moveDirection.y = movementDirectionY;
+        }
+
+        //If in air let gravity pull the characters down
+        if (!characterController.isGrounded)
+        {
+            moveDirection.y -= gravity * Time.deltaTime;
+        }
+
+  
+
+        #endregion
+
+        #region Handles Rotation
+
+        characterController.Move(moveDirection * Time.deltaTime);
+
+        if(canMove)
+        {
+            rotationX += -Input.GetAxis("Mouse Y") * lookSpeed;  
+            rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);  //Prevents camera from rotating too far
+            playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0); //Rotate the cam
+            transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);  //Rotates the player left and right
+        
+        }
+        #endregion
+
+    }
+
+    
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,20 +13,29 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Cursor.lockState = CursorLockMode.Locked;
         controller = gameObject.AddComponent<CharacterController>();
-        speed = 2f;
+        speed = 4f;
     }
 
     // Update is called once per frame
     void Update()
     {
+        float x_input = Input.GetAxis("Horizontal");
+        float z_input = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x_input + transform.forward * z_input;  //Calculate the movement
+
+        controller.Move(Vector3.ClampMagnitude(move, 1f) * speed * Time.deltaTime);
+
+
         bool isGrounded = controller.isGrounded;
         if(isGrounded && playerVelocity.y < 0){
             playerVelocity.y = 0f;
         }
 
-        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        controller.Move(move * Time.deltaTime * speed);
+        //Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        //controller.Move(move * Time.deltaTime * speed);
 
         // if(move != Vector3.zero){
         //     gameObject.transform.forward = move;
